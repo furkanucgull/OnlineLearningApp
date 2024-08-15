76d566b15d1cfca05df0d6317e5df385a2edd73e
furkanucgull <furkanucgull@gmail.com>
2024-08-15 19:57:08 +0200

    Update Register Formik

--- frontend/QuranPath/src/pages/Register.tsx
+++ frontend/QuranPath/src/pages/Register.tsx
@@ -1,31 +1,11 @@
 import axios from "axios";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { API_BASE_URL } from "../apiConfig";
 import { showToast } from '../components/Toast';
-
-// const ToastContent = ({ message }: { message: string; }) => (
-//     <div className="flex items-center bg-blue-500 text-white p-3 rounded-lg shadow-md">
-//         <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-//             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h1m-1-4h.01m-6.93 12A9.986 9.986 0 0112 3a9.986 9.986 0 017.93 4.55A9.986 9.986 0 0121 12a9.986 9.986 0 01-1.07 4.45A9.986 9.986 0 0112 21a9.986 9.986 0 01-6.93-2.45z" />
-//         </svg>
-//         <span>{message}</span>
-//     </div>
-// );
-
-// const showToast = (message: string) => {
-//     toast(<ToastContent message={message} />, {
-//         position: "top-right",
-//         autoClose: 5000,
-//         hideProgressBar: true,
-//         closeOnClick: true,
-//         pauseOnHover: true,
-//         draggable: true,
-//         progress: undefined,
-//     });
-// };
+import { useFormik } from 'formik';
+import { RegisterFormSchema } from '../schemas/RegisterFormSchemas';  // Şema dosyasını içe aktar
 
 const calculateAge = (dateOfBirth: string) => {
     const birthDate = new Date(dateOfBirth);
@@ -41,94 +21,157 @@ const calculateAge = (dateOfBirth: string) => {
 const Register = () => {
     const navigate = useNavigate();
 
-    const [name, setName] = useState<string>("");
-    const [parentName, setParentName] = useState<string>("");
-    const [parentPhone, setParentPhone] = useState<string>("");
-    //const [username, setUserName] = useState("");
-    const [surname, setSurname] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [dateOfBirth, setDateOfBirth] = useState<string>("");
-    const [age, setAge] = useState<number | null>(null);
-
-    const handleDateOfBirthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const dob = e.target.value;
-        setDateOfBirth(dob);
-        const calculatedAge = calculateAge(dob);
-        setAge(calculatedAge);
-    };
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const payload = {
-            username: "",
-            parentName,
-            parentPhone,
-            password,
-            name,
-            surname,
-            email,
-            age,
-        };
-        try {
-            await axios.post(`${API_BASE_URL}/Registers`, payload);
-            showToast("Kayıt Başarılı, Giriş Sayfasına Yönlendiriliyorsunuz");
-            setTimeout(() => {
-                navigate("/login");
-            }, 5000);
-        } catch (error) {
-            toast.warning("Kayıt sırasında bir hata oluştu.");
-            console.log("Hata", error);
-        }
-    };
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            surname: "",
+            parentName: "",
+            parentPhone: "",
+            dateOfBirth: "",
+            email: "",
+            password: "",
+        },
+        validationSchema: RegisterFormSchema,
+        onSubmit: async (values) => {
+            const age = calculateAge(values.dateOfBirth);
+            const payload = {
+                username: "",
+                parentName: values.parentName,
+                parentPhone: values.parentPhone,
+                password: values.password,
+                name: values.name,
+                surname: values.surname,
+                email: values.email,
+                age,
+            };
+            try {
+                await axios.post(`${API_BASE_URL}/Registers`, payload);
+                showToast("Kayıt Başarılı, Giriş Sayfasına Yönlendiriliyorsunuz");
+                setTimeout(() => {
+                    navigate("/login");
+                }, 5000);
+            } catch (error) {
+                toast.warning("Kayıt sırasında bir hata oluştu.");
+                console.log("Hata", error);
+            }
+        },
+    });
 
     return (
         <div className="max-w-lg mx-auto bg-white dark:bg-gray-800 rounded-lg shadow-md px-8 py-10 flex flex-col items-center">
             <h1 className="text-xl font-bold text-center text-gray-700 dark:text-gray-200 mb-8">Kayıt Formu</h1>
-            <form onSubmit={(e) => handleSubmit(e)} className="w-full flex flex-col gap-4">
+            <form onSubmit={formik.handleSubmit} className="w-full flex flex-col gap-4">
                 <div className="flex items-start flex-col justify-start">
                     <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Öğrenci İsim :</label>
-                    <input value={name} onChange={(e) => setName(e.target.value)} type="text" id="firstName" name="firstName" className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    />
+                    {formik.touched.name && formik.errors.name ? <div className="text-red-500 text-sm">{formik.errors.name}</div> : null}
                 </div>
 
                 <div className="flex items-start flex-col justify-start">
                     <label className="text-sm text-gray-700 dark:text-gray-200 mr-2"> Öğrenci Soyisim :</label>
-                    <input value={surname} onChange={(e) => setSurname(e.target.value)} type="text" id="lastName" name="lastName" className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
+                    <input
+                        type="text"
+                        id="surname"
+                        name="surname"
+                        value={formik.values.surname}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    />
+                    {formik.touched.surname && formik.errors.surname ? <div className="text-red-500 text-sm">{formik.errors.surname}</div> : null}
                 </div>
+
                 <div className="flex items-start flex-col justify-start">
                     <label className="text-sm text-gray-700 dark:text-gray-200 mr-2"> Veli İsim Soyisim :</label>
-                    <input value={parentName} onChange={(e) => setParentName(e.target.value)} type="text" id="lastName" name="lastName" className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
+                    <input
+                        type="text"
+                        id="parentName"
+                        name="parentName"
+                        value={formik.values.parentName}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    />
+                    {formik.touched.parentName && formik.errors.parentName ? <div className="text-red-500 text-sm">{formik.errors.parentName}</div> : null}
                 </div>
+
                 <div className="flex items-start flex-col justify-start">
                     <label className="text-sm text-gray-700 dark:text-gray-200 mr-2"> Veli Tel No :</label>
-                    <input value={parentPhone} onChange={(e) => setParentPhone(e.target.value)} type="tel" id="lastName" name="lastName" className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
+                    <input
+                        type="tel"
+                        id="parentPhone"
+                        name="parentPhone"
+                        value={formik.values.parentPhone}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    />
+                    {formik.touched.parentPhone && formik.errors.parentPhone ? <div className="text-red-500 text-sm">{formik.errors.parentPhone}</div> : null}
                 </div>
 
                 <div className="flex items-start flex-col justify-start">
                     <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Öğrenci Doğum Tarihi :</label>
-                    <input type="date" value={dateOfBirth} onChange={handleDateOfBirthChange} className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
+                    <input
+                        type="date"
+                        id="dateOfBirth"
+                        name="dateOfBirth"
+                        value={formik.values.dateOfBirth}
+                        onChange={(e) => {
+                            formik.handleChange(e);
+                            const dob = e.target.value;
+                            const calculatedAge = calculateAge(dob);
+                            formik.setFieldValue("dateOfBirth", dob);
+                            formik.setFieldValue("age", calculatedAge);
+                        }}
+                        onBlur={formik.handleBlur}
+                        className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    />
+                    {formik.touched.dateOfBirth && formik.errors.dateOfBirth ? <div className="text-red-500 text-sm">{formik.errors.dateOfBirth}</div> : null}
                 </div>
 
                 <div className="flex items-start flex-col justify-start">
-                    <label className="text-sm text-gray-700 dark:text-gray-200 mr-2"> Öğrenci Email :</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email" className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
+                    <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Öğrenci Email :</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    />
+                    {formik.touched.email && formik.errors.email ? <div className="text-red-500 text-sm">{formik.errors.email}</div> : null}
                 </div>
-
-
 
                 <div className="flex items-start flex-col justify-start">
                     <label className="text-sm text-gray-700 dark:text-gray-200 mr-2">Şifreniz :</label>
-                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500" />
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        className="w-full px-3 dark:text-gray-200 dark:bg-gray-900 py-2 rounded-md border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    />
+                    {formik.touched.password && formik.errors.password ? <div className="text-red-500 text-sm">{formik.errors.password}</div> : null}
                 </div>
 
-                <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-md shadow-sm">Kayıt Ol</button>
+                <button
+                    type="submit"
+                    className="mt-6 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+                >
+                    Kayıt Ol
+                </button>
             </form>
-
-            <div className="mt-4 text-center">
-                <span className="text-sm text-gray-500 dark:text-gray-300">Zaten hesabınız var mı? </span>
-                <a onClick={() => navigate("/login")} className="text-blue-500 hover:text-blue-600">Giriş Yapın</a>
-            </div>
-
             <ToastContainer />
         </div>
     );
