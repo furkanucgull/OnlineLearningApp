49961c0b9e2890a57b25f00f5bceaf86b45379c8
furkanucgull <furkanucgull@gmail.com>
2024-08-19 15:34:44 +0200

    publish

--- api/OnlineLearningApi/OnlineLearningApi/Program.cs
+++ api/OnlineLearningApi/OnlineLearningApi/Program.cs
@@ -6,6 +6,7 @@ using OnlineLearningApi.Persistence.Context;
 using OnlineLearningApi.Persistence.Repositories;
 using OnlineLearningApi.Application.Services;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace OnlineLearningApi
 {
@@ -14,19 +15,7 @@ namespace OnlineLearningApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
-            //{
-            //    opt.RequireHttpsMetadata = false;
-            //    opt.TokenValidationParameters = new TokenValidationParameters
-            //    {
-            //        ValidAudience = JWTTokenDefaults.ValidAudience,
-            //        ValidIssuer = JWTTokenDefaults.ValidIssuer,
-            //        ClockSkew = TimeSpan.Zero,
-            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JWTTokenDefaults.Key)),
-            //        ValidateLifetime = true,
-            //        ValidateIssuerSigningKey = true,
-            //    };
-            //});
+
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
              .AddJwtBearer(opt =>
              {
@@ -41,16 +30,7 @@ namespace OnlineLearningApi
                      ValidateIssuerSigningKey = true,
                  };
              });
-            //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddCookie(JwtBearerDefaults.AuthenticationScheme, opt =>
-            //{
-            //    opt.LoginPath = "/Login/Index/";
-            //    opt.LogoutPath = "/Login/LogOut/";
-            //    opt.AccessDeniedPath = "/Pages/AccessDenied/";
-            //    opt.Cookie.SameSite = SameSiteMode.Strict;
-            //    opt.Cookie.HttpOnly = true;
-            //    opt.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
-            //    opt.Cookie.Name = "OnlineLearningJWT";
-            //});
+
             //registry
             builder.Services.AddScoped<AppDbContext>();
             builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
@@ -60,7 +40,8 @@ namespace OnlineLearningApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
